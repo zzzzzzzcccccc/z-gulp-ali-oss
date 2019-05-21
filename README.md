723f4b1771063170c5e8e2bddb890d7c3266f1f5
zc <z304307393@163.com>
2019-05-21 10:05:51 +0800

    first

---
+++ README.md
@@ -0,0 +1,38 @@
+# z-gulp-ali-oss
+Aliyun Oss Client for Gulp
+
+### Install
+``` bash
+npm install z-gulp-ali-oss -D
+
+### DEMO
+``` node
+var gulp = require('gulp');
+var oss = require('gulp-alioss');
+gulp.task('oss', function(){
+    var options = {
+        accessKeyId: '********',
+        secretAccessKey: '*********',
+        endpoint: 'http://oss-cn-***.aliyuncs.com',
+        apiVersion: '2013-10-15',
+        prefix: 'assets/js', //for no prefix: prefix: ''
+        bucket: 'test'
+    };
+    //./js/a.js -> <prefix>/a.js
+    // ......
+    //./js/b/a.js -> <prefix>/b/a.js
+    return gulp.src(['./js/**/*']).pipe(oss(options));
+});
+```
+### 操作结果
+
+``` bash
+cd /path/to/gulpfile.js/
+gulp oss
+[14:50:59] Using gulpfile ~/path/to/gulpfile.js
+[14:50:59] Starting 'oss'...
+[14:50:59] Finished 'oss' after *** ms
+[14:50:59] OK: assets/a.js
+[14:50:59] OK: assets/b/a.js
+....
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+// PLUGIN_NAME: gulp-oss
+const PLUGIN_NAME = 'gulp-oss';
+
+var path = require('path');
+var through2 = require('through2');
+var PluginError = require('gulp-util').PluginError;
+var colors = require('gulp-util').colors;
+var log = require('gulp-util').log;
+var ALY = require('aliyun-sdk');
+var Moment = require('moment');
+var Q = require('q');
+var mime = require('mime');
+
+function oss(option) {
+  if (!option) {
+    throw new PluginError(PLUGIN_NAME, 'Missing option!');
+  }
+  if(!option.bucket){
+    throw new PluginError(PLUGIN_NAME, 'Missing option.bucket!');
+  }
+
+  //var ossClient = new ALY.OSS({
+  //    accessKeyId: option.accessKeyId,
+  //    secretAccessKey: option.secretAccessKey,
+  //    endpoint: option.endpoint,
+  //    apiVersion: option.apiVersion
+  //});
+
+  var version = Moment().format('YYMMDDHHmm');
+
+  return through2.obj(function (file, enc, cb) {
+    if(file.isDirectory()) return cb();
+    if(file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+      return cb();
+    }
+    if(file.contents.length >= option.maxSize){
+      log('WRN:', colors.red(file.path + "\t" + file.contents.length));
+      return cb();
+    }
+    var getFileKey = function(){
+      return option.prefix
+        + ((!option.prefix || (option.prefix[option.prefix.length - 1]) === '/') ? '' : '/')
+        + (option.versioning ? version + '/' : '')
+        + path.relative(file.base, file.path);
+    };
+    var uploadFile = function(fileKey){
+      ossClient = new ALY.OSS({
+        accessKeyId: option.accessKeyId,
+        secretAccessKey: option.secretAccessKey,
+        endpoint: option.endpoint,
+        apiVersion: option.apiVersion
+      });
+      ossClient.putObject({
+          Bucket: option.bucket,
+          Key: fileKey,
+          Body: file.contents,
+          AccessControlAllowOrigin: '',
+          CacheControl: '',
+          ContentDisposition: '',
+          ContentEncoding: 'utf-8',
+          ServerSideEncryption: 'AES256',
+          ContentType: mime.getType(file.path),
+        }, function (err, data) {
+          if (err) {
+            console.log('error:', err);
+            log('ERR:', colors.red(fileKey + "\t" + err.code));
+          }else{
+            log('OK:', colors.green(fileKey));
+          }
+        }
+      );
+    };
+    Q.fcall(getFileKey).then(uploadFile);
+    this.push(file);
+    return cb();
+  });
+}
+
+module.exports = oss;
